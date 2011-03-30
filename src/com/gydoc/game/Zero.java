f6e0e91906d198b4d48aac9187fc888ab3cb441b
kjsimple <kjsimple@gmail.com>
2011-03-30 22:00:12 +0800

    test

---
+++ src/com/gydoc/game/Zero.java
@@ -0,0 +1,16 @@
+package com.gydoc.game;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.phonegap.DroidGap;
+
+public class Zero extends DroidGap {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
