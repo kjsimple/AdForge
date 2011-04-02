42a9b35bedfbd5888ec5ac896f24ac826bc347f8
kjsimple <kjsimple@gmail.com>
2011-04-02 16:05:51 +0800

    remove phonegap and jo

--- src/com/gydoc/game/Zero.java
+++ src/com/gydoc/game/Zero.java
@@ -2,15 +2,13 @@ package com.gydoc.game;
 
 import android.app.Activity;
 import android.os.Bundle;
-import com.phonegap.DroidGap;
 
-public class Zero extends DroidGap {
+public class Zero extends Activity {
     /**
      * Called when the activity is first created.
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.loadUrl("file:///android_asset/www/index.html");
     }
 }
